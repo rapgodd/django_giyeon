e74c55845e441d50b736c493de1b95a62d9a6296
giyeon <bdcgy22@daum.net>
2024-02-08 11:35:43 +0900

    CBV로 디테일 페이지 만들기

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     #path('', views.index), #FBV를 할때에는 이렇게 적는다.
-    path('<int:pk>/', views.single_post_page),
+    path('<int:pk>/', views.PostDetail.as_view()),
     path('', views.PostList.as_view()),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from .models import Post
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 
 class PostList(ListView):
     model = Post
     ordering = '-pk'
+
+class PostDetail(DetailView):
+    model = Post
+    
+
 
 # def index(request):
 #     posts = Post.objects.all().order_by('-pk')
@@ -13,13 +18,13 @@ class PostList(ListView):
 #     return render(request, 'blog/index.html', {'posts': posts})
 #
 
-def single_post_page(request, pk):
-    post = Post.objects.get(pk=pk)
-
-    return render(
-        request,
-        'blog/single_page.html',
-        {
-            'post': post,
-        }
-    )
+# def single_post_page(request, pk):
+#     post = Post.objects.get(pk=pk)
+#
+#     return render(
+#         request,
+#         'blog/single_page.html',
+#         {
+#             'post': post,
+#         }
+#     )
