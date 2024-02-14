9e8432f068eed364d650ff65d420a39f8e1b41f4
giyeon <bdcgy22@daum.net>
2024-02-14 21:20:52 +0900

    Post와 다대일 관계인 카테고리 모델 생성

--- blog/models.py
+++ blog/models.py
@@ -2,12 +2,26 @@ from django.db import models
 from django.contrib.auth.models import User
 import os
 
+
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=50, unique=True, allow_unicode=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+
 class Post(models.Model):
     title = models.CharField(max_length=50)
     content = models.TextField()
     hook_text = models.CharField(max_length=100, blank=True)
-    author = models.ForeignKey(User, null=True ,on_delete=models.SET_NULL)
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
 
     head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True)
     file_upload = models.FileField(upload_to='blog/files/%Y/%m/%d/', blank=True)
