0cfa64dc7fd55c36d54e58a37c31c3182eff5d61
giyeon <bdcgy22@daum.net>
2024-02-14 11:07:19 +0900

    다대일 관계로 모델들을 연결하기

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 import os
 
 # Create your models here.
@@ -6,6 +7,7 @@ class Post(models.Model):
     title = models.CharField(max_length=50)
     content = models.TextField()
     hook_text = models.CharField(max_length=100, blank=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True)
     file_upload = models.FileField(upload_to='blog/files/%Y/%m/%d/', blank=True)
@@ -13,7 +15,7 @@ class Post(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f'[{self.pk}] {self.title}'
+        return f'[{self.pk}] {self.title} :: {self.author}'
 
     def get_absolute_url(self):
         return f'/blog/{self.pk}/'
