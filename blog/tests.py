3a0c1949fc87589b49be7a81e5f170f255623d29
giyeon <bdcgy22@daum.net>
2024-02-16 12:04:14 +0900

    포스트 디테일 페이지에 태그 추가하기

--- blog/tests.py
+++ blog/tests.py
@@ -165,6 +165,9 @@ class TestView(TestCase):
         post_area = main_area.find('div', id='post-area')
         self.assertIn(self.post_001.title, post_area.text)
         self.assertIn(self.post_001.category.name, post_area.text)
+        self.assertIn(self.tag_hello.name, post_area.text)
+        self.assertNotIn(self.tag_python.name, post_area.text)
+        self.assertNotIn(self.tag_python_kor.name, post_area.text)
 
         # 2.5. 첫 번째 포스트의 작성자(author)가 포스트 영역에 있다(아직 구현할 수 없음).
         self.assertIn(self.post_001.author.username.upper(), post_area.text)
