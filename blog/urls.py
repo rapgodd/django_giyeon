e41facf2cccbc6c642f5552d32b9035be13e6cf9
giyeon <bdcgy22@daum.net>
2024-02-17 12:44:11 +0900

    작성자랑 요청자가 같은 경우에만 포스트 수정할 수 있도록 하기

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     #path('', views.index), #FBV를 할때에는 이렇게 적는다.
+    path('update_post/<int:pk>/', views.PostUpdate.as_view()),
     path('create_post/', views.PostCreate.as_view()),
     path('tag/<str:slug>/', views.tag_page),
     path('category/<str:slug>/', views.category_page),
