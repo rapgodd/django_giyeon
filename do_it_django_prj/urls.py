0071d3e2e4db0fc14d064e1a0b588c116d7c749c
giyeon <bdcgy22@daum.net>
2024-03-04 12:48:19 +0900

    gunicorn설정

--- do_it_django_prj/urls.py
+++ do_it_django_prj/urls.py
@@ -27,3 +27,4 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
